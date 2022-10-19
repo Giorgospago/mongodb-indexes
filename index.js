b76a0407c77c99c5b1bacaada568229ba43fab57
zartaz <kzartas@gmail.com>
2022-10-19 15:29:41 +0300

    mongo.close()

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 async function main() {
+    const skipIndexes = ["_id_"];
+
+    const exportClient = new MongoClient(process.env.EXPORT_URI);
+    await exportClient.connect();
+    const exportDb = exportClient.db();
+
+    const importClient = new MongoClient(process.env.IMPORT_URI);
+    await importClient.connect();
+    const importDb = importClient.db();
+
     try {
-        const skipIndexes = ["_id_"];
-
-        const exportClient = new MongoClient(process.env.EXPORT_URI);
-        await exportClient.connect();
-        const exportDb = exportClient.db();
-
-        const importClient = new MongoClient(process.env.IMPORT_URI);
-        await importClient.connect();
-        const importDb = importClient.db();
-
-        const promiseIndexes = [];
         const collections = await exportDb.collections();
         for (const collection of collections) {
             try {
@@ -22,9 +21,8 @@ async function main() {
                     if (!skipIndexes.includes(index.name)) {
                         const existingIndexes = await importDb.collection(collection.collectionName).indexes();
                         if (!existingIndexes.find(i => i.name === index.name)) {
-                            console.log(collection.collectionName, index.name, index.key);
-                            promiseIndexes.push(
-                                importDb
+                            try {
+                                await importDb
                                     .collection(collection.collectionName)
                                     .createIndex(
                                         index.key,
@@ -32,21 +30,31 @@ async function main() {
                                             name: index.name,
                                             background: true
                                         }
-                                    )
-                            )
+                                    );
+                                console.log(collection.collectionName, index.name, index.key);
+                            } catch (e) {
+                                if (e.message.indexOf("wildcard")) {
+                                    console.error("Wildcard: ", collection.collectionName, index.name);
+                                }
+                                if (e.message.indexOf("ns does not exist")) {
+                                    console.error("Collection: " + collection.collectionName + " does not exist");
+                                }
+                            }
                         }
                     }
                 }
             } catch (e) {
-                console.log(e.message);
+                if (e.message.indexOf("ns does not exist")) {
+                    console.error("Collection: " + collection.collectionName + " does not exist");
+                }
             }
         }
-
-        await Promise.all(promiseIndexes);
-        console.log("\n\nINDEXES IMPORTED SUCCESSFULLY\n\n");
-
     } catch (e) {
         console.log(e.message);
     }
+    await exportClient.close();
+    await importClient.close();
+    console.log("\n\nINDEXES IMPORTED SUCCESSFULLY\n\n");
 }
+
 main();
